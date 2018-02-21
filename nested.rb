d96d796e9b6a7360c6a20b45beed41fa347bb842
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-21 17:24:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].first
+		 programmer_hash[:dennis_ritchie][:languages].to_s
+
 end
 
 def adding_matz
